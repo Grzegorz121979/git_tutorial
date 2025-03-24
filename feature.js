974f1e9642f048f0a79304ab85764eca46c0dc4a
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-03-24 20:51:15 +0100

    Updated feature.js

--- feature.js
+++ feature.js
@@ -1,6 +1,7 @@
 let name = document.getElementById("username");
 let button = document.getElementById("my_button");
+let label = document.getElementById("my_label");
 
 button.addEventListener('click', () => {
-	console.log("Hello, " + name.value);
+	label.textContent = name.value
 });
